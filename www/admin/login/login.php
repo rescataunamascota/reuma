c07bf2c389722642c68d5e6d1d56b762b1ad6b42
jchpoli <57495086+jchpoli@users.noreply.github.com>
2019-12-15 18:42:01 -0500

    Se corrige inclusión de css de boostrap

--- www/admin/login/login.php
+++ www/admin/login/login.php
@@ -5,15 +5,14 @@
 <!--[if gt IE 8]><!-->
 <html class="no-js">
 <!--<![endif]-->
-<head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-<title>PHP Login Form</title>
-<meta name="description" content="">
-<meta name="viewport" content="width=device-width">
-<link rel="stylesheet" href="css/bootstrap.css">
-<link rel="stylesheet" href="css/main.css">
 
+<head>
+   <meta charset="utf-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+   <title>PHP Login Form</title>
+   <meta name="description" content="">
+   <meta name="viewport" content="width=device-width">
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
 </head>
 <!-- NAVBAR
@@ -21,69 +20,66 @@
 
 <body>
 
-<div class="container">
-  <div class="row" style="margin-top:20px">
-    <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
-      <form name="form_login" method="post" action="login.php" role="form">
-        <fieldset>
-          <h2>Please Sign In</h2>
-          <hr class="colorgraph">
-          <div class="form-group">
-            <input name="user_id" type="text" id="email" class="form-control input-lg" placeholder="Email Address">
-          </div>
-          <div class="form-group">
-            <input type="password" name="password" id="clave" class="form-control input-lg" placeholder="Password">
-          </div>
-          <span class="button-checkbox">
-          <button type="button" class="btn" data-color="info">Remember Me</button><!-- Additional Option -->
-          <input type="checkbox" name="remember_me" id="remember_me" checked="checked" class="hidden">
-          <hr class="colorgraph">
-          <div class="row">
-            <div class="col-xs-6 col-sm-6 col-md-6">
-              <input type="submit" name="Submit" value="Login" class="btn btn-lg btn-success btn-block">
-            </div>
-            <div class="col-xs-6 col-sm-6 col-md-6"> <a href="http://creativealive.com/basic-registration-form-php-mysql-database-connectivity/" target="_blank" class="btn btn-lg btn-primary btn-block">Register<small>- Read Article</small></a> </div>
-          </div>
-        </fieldset>
-      </form>
-    </div>
-  </div>
-</div>
+   <div class="container">
+      <div class="row" style="margin-top:20px">
+         <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
+            <form name="form_login" method="post" action="login.php" role="form">
+               <fieldset>
+                  <h2>Please Sign In</h2>
+                  <hr class="colorgraph">
+                  <div class="form-group">
+                     <input name="user_id" type="text" id="email" class="form-control input-lg" placeholder="Email Address">
+                  </div>
+                  <div class="form-group">
+                     <input type="password" name="password" id="clave" class="form-control input-lg" placeholder="Password">
+                  </div>
+                  <span class="button-checkbox">
+                     <button type="button" class="btn" data-color="info">Remember Me</button><!-- Additional Option -->
+                     <input type="checkbox" name="remember_me" id="remember_me" checked="checked" class="hidden">
+                     <hr class="colorgraph">
+                     <div class="row">
+                        <div class="col-xs-6 col-sm-6 col-md-6">
+                           <input type="submit" name="Submit" value="Login" class="btn btn-lg btn-success btn-block">
+                        </div>
+                        <div class="col-xs-6 col-sm-6 col-md-6"> <a href="http://creativealive.com/basic-registration-form-php-mysql-database-connectivity/" target="_blank" class="btn btn-lg btn-primary btn-block">Register<small>- Read Article</small></a> </div>
+                     </div>
+               </fieldset>
+            </form>
+         </div>
+      </div>
+   </div>
 
 
 
-<?php     //start php tag
-//include connect.php page for database connection
-Include('connect.php');
-//if submit is not blanked i.e. it is clicked.
-if (isset($_REQUEST['Submit'])) //here give the name of your button on which you would like    //to perform action.
-{
-// here check the submitted text box for null value by giving there name.
-	if($_REQUEST['email']=="" || $_REQUEST['clave']=="")
-	{
-	echo " Field must be filled";
-	}
-	else
-	{
-	   $sql1= "select * from admins where email= '".$_REQUEST['email']."' &&  password ='".$_REQUEST['clave']."'";
-	  $result=mysql_query($sql1)
-	    or exit("Sql Error".mysql_error());
-	    $num_rows=mysql_num_rows($result);
-	   if($num_rows>0)
-	   {
-//here you can redirect on your file which you want to show after login just change filename ,give it to your filename.
-		   //header("location:filename.php"); 
- //OR just simply print a message.
-         Echo "You have logged in successfully";	
-        }
-	    else
-		{
-			echo "username or password incorrect";
-		}
-	}
-}	
-?>
+   <?php     //start php tag
+   //include connect.php page for database connection
+   include('connect.php');
+   //if submit is not blanked i.e. it is clicked.
+   if (isset($_REQUEST['Submit'])) //here give the name of your button on which you would like    //to perform action.
+   {
+      // here check the submitted text box for null value by giving there name.
+      if ($_REQUEST['email'] == "" || $_REQUEST['clave'] == "") {
+         echo " Field must be filled";
+      } else {
+         $sql1 = "select * from admins where email= '" . $_REQUEST['email'] . "' &&  password ='" . $_REQUEST['clave'] . "'";
+         $result = mysql_query($sql1)
+            or exit("Sql Error" . mysql_error());
+         $num_rows = mysql_num_rows($result);
+         if ($num_rows > 0) {
+            //here you can redirect on your file which you want to show after login just change filename ,give it to your filename.
+            //header("location:filename.php"); 
+            //OR just simply print a message.
+            echo "You have logged in successfully";
+         } else {
+            echo "username or password incorrect";
+         }
+      }
+   }
+   ?>
 
-
+   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 </body>
+
 </html>
